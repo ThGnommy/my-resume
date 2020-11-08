dd88079bd9b1436600d18a7150119b55101c309a
ThGnommy <th.brandoli@gmail.com>
2020-11-08 23:56:22 +0100

    add my pexels

--- src/components/PersonalProjects/PersonalProjects.js
+++ src/components/PersonalProjects/PersonalProjects.js
@@ -1,25 +1,64 @@
-import React from 'react'
-import { Box, Typography } from '@material-ui/core'
-import {MATCH_GAME, WEATHER_APP, BOOK_APP, PORTFOLIO_SAMPLE, LOL_BOT, REACT_TO_DO, SOCIAL_MEDIA_DASHBOARD} from './projects'
-import {SingleProject} from './SingleProject'
-import './PersonalProjects.css'
+import React from "react";
+import { Box, Typography } from "@material-ui/core";
+import {
+  MATCH_GAME,
+  WEATHER_APP,
+  BOOK_APP,
+  PORTFOLIO_SAMPLE,
+  LOL_BOT,
+  REACT_TO_DO,
+  SOCIAL_MEDIA_DASHBOARD,
+  MY_PEXELS,
+} from "./projects";
+import { SingleProject } from "./SingleProject";
+import "./PersonalProjects.css";
 
 export const PersonalProjects = () => {
-
-    return (
-  <div>
-    <Box boxShadow={3} className="project-tab" p={3} mt={3}>
-
-    <Typography variant="h6">Personal Project</Typography>
-    <SingleProject title={WEATHER_APP.title} text={WEATHER_APP.text} link={WEATHER_APP.link} />
-    <SingleProject title={BOOK_APP.title} text={BOOK_APP.text} link={BOOK_APP.link} />
-    <SingleProject title={PORTFOLIO_SAMPLE.title} text={PORTFOLIO_SAMPLE.text} link={PORTFOLIO_SAMPLE.link} />
-    <SingleProject title={REACT_TO_DO.title} text={REACT_TO_DO.text} link={REACT_TO_DO.link} />
-    <SingleProject title={LOL_BOT.title} text={LOL_BOT.text} link={LOL_BOT.link} />
-    <SingleProject title={MATCH_GAME.title} text={MATCH_GAME.text} link={MATCH_GAME.link} />
-    <SingleProject title={SOCIAL_MEDIA_DASHBOARD.title} text={SOCIAL_MEDIA_DASHBOARD.text} link={SOCIAL_MEDIA_DASHBOARD.link} />
-    
-    </Box>
-  </div>
-    )
-}
+  return (
+    <div>
+      <Box boxShadow={3} className="project-tab" p={3} mt={3}>
+        <Typography variant="h6">Personal Project</Typography>
+        <SingleProject
+          title={WEATHER_APP.title}
+          text={WEATHER_APP.text}
+          link={WEATHER_APP.link}
+        />
+        <SingleProject
+          title={MY_PEXELS.title}
+          text={MY_PEXELS.text}
+          link={MY_PEXELS.link}
+        />
+        <SingleProject
+          title={BOOK_APP.title}
+          text={BOOK_APP.text}
+          link={BOOK_APP.link}
+        />
+        <SingleProject
+          title={PORTFOLIO_SAMPLE.title}
+          text={PORTFOLIO_SAMPLE.text}
+          link={PORTFOLIO_SAMPLE.link}
+        />
+        <SingleProject
+          title={REACT_TO_DO.title}
+          text={REACT_TO_DO.text}
+          link={REACT_TO_DO.link}
+        />
+        <SingleProject
+          title={LOL_BOT.title}
+          text={LOL_BOT.text}
+          link={LOL_BOT.link}
+        />
+        <SingleProject
+          title={MATCH_GAME.title}
+          text={MATCH_GAME.text}
+          link={MATCH_GAME.link}
+        />
+        <SingleProject
+          title={SOCIAL_MEDIA_DASHBOARD.title}
+          text={SOCIAL_MEDIA_DASHBOARD.text}
+          link={SOCIAL_MEDIA_DASHBOARD.link}
+        />
+      </Box>
+    </div>
+  );
+};
--- src/components/PersonalProjects/projects.js
+++ src/components/PersonalProjects/projects.js
@@ -1,88 +1,140 @@
-import React from 'react'
-import {Typography, Link} from '@material-ui/core'
+import React from "react";
+import { Typography, Link } from "@material-ui/core";
 
 export const MATCH_GAME = {
-    title: 'Match Game',
-    text: 'Memory game creato utilizzando jQuery.',
-    link: <Typography>
-                <Link style={{color: "#fff", textDecoration: "underline"}} 
-                href="https://thgnommy.github.io/Match-Game/" 
-                rel="noopener noreferrer" 
-                target="_blank">
-                    Link
-                </Link>
-            </Typography>}
+  title: "Match Game",
+  text: "Memory game creato utilizzando jQuery.",
+  link: (
+    <Typography>
+      <Link
+        style={{ color: "#fff", textDecoration: "underline" }}
+        href="https://thgnommy.github.io/Match-Game/"
+        rel="noopener noreferrer"
+        target="_blank"
+      >
+        Link
+      </Link>
+    </Typography>
+  ),
+};
+
+export const MY_PEXELS = {
+  title: "My Pexels",
+  text:
+    "Applicazione che ti permette di fare ricerche e di salvare e scaricare immaginI, con l'utilizzo delle API di pexels.com",
+  link: (
+    <Typography>
+      <Link
+        style={{ color: "#fff", textDecoration: "underline" }}
+        href="https://my-pexels.netlify.app/"
+        rel="noopener noreferrer"
+        target="_blank"
+      >
+        Link
+      </Link>
+    </Typography>
+  ),
+};
 
 export const WEATHER_APP = {
-    title: 'Another Weather Application',
-    text: 'Un\'applicazione che ti permette di ottenere le previsioni meteo di una città. Sviluppato con webpack e integrando le API di geolocalizzazione.',
-    link: <Typography>
-                <Link style={{color: "#fff", textDecoration: "underline"}} 
-                href="https://another-weather-application.netlify.app/" 
-                rel="noopener noreferrer" 
-                target="_blank">
-                    Link
-                </Link>
-            </Typography>
-}
+  title: "Another Weather Application",
+  text:
+    "Un'applicazione che ti permette di ottenere le previsioni meteo di una città. Sviluppato con webpack e integrando le API di geolocalizzazione.",
+  link: (
+    <Typography>
+      <Link
+        style={{ color: "#fff", textDecoration: "underline" }}
+        href="https://another-weather-application.netlify.app/"
+        rel="noopener noreferrer"
+        target="_blank"
+      >
+        Link
+      </Link>
+    </Typography>
+  ),
+};
+
 export const BOOK_APP = {
-    title: 'Google Book Finder',
-    text: 'Applicazione React (+ Redux) utilizzando le API di Google Books e Material UI.',
-    link: <Typography>
-                <Link style={{color: "#fff", textDecoration: "underline"}} 
-                href="https://th-book-finder.netlify.app/" 
-                rel="noopener noreferrer" 
-                target="_blank">
-                    Link
-                </Link>
-            </Typography>
-}
+  title: "Google Book Finder",
+  text:
+    "Applicazione React (+ Redux) utilizzando le API di Google Books e Material UI.",
+  link: (
+    <Typography>
+      <Link
+        style={{ color: "#fff", textDecoration: "underline" }}
+        href="https://th-book-finder.netlify.app/"
+        rel="noopener noreferrer"
+        target="_blank"
+      >
+        Link
+      </Link>
+    </Typography>
+  ),
+};
 export const PORTFOLIO_SAMPLE = {
-    title: 'Portfolio Example',
-    text: 'Esempio di un sito portfolio utilizzando solo HTML e CSS.',
-    link: <Typography>
-                <Link style={{color: "#fff", textDecoration: "underline"}} 
-                href="https://th-book-finder.netlify.app/" 
-                rel="noopener noreferrer" 
-                target="_blank">
-                    Link
-                </Link>
-            </Typography>
-}
+  title: "Portfolio Example",
+  text: "Esempio di un sito portfolio utilizzando solo HTML e CSS.",
+  link: (
+    <Typography>
+      <Link
+        style={{ color: "#fff", textDecoration: "underline" }}
+        href="https://th-book-finder.netlify.app/"
+        rel="noopener noreferrer"
+        target="_blank"
+      >
+        Link
+      </Link>
+    </Typography>
+  ),
+};
 export const LOL_BOT = {
-    title: 'League of Leagend Discord Bot',
-    text: 'Sviluppato in javascript, questo bot ti permette di avere informazioni su qualsiasi giocatore di League of Leagends tramite API.',
-    link: <Typography>
-                <Link style={{color: "#fff", textDecoration: "underline"}} 
-                href="https://github.com/ThGnommy/lol-discord-bot" 
-                rel="noopener noreferrer" 
-                target="_blank">
-                    Link
-                </Link>
-            </Typography>
-}
+  title: "League of Leagend Discord Bot",
+  text:
+    "Sviluppato in javascript, questo bot ti permette di avere informazioni su qualsiasi giocatore di League of Leagends tramite API.",
+  link: (
+    <Typography>
+      <Link
+        style={{ color: "#fff", textDecoration: "underline" }}
+        href="https://github.com/ThGnommy/lol-discord-bot"
+        rel="noopener noreferrer"
+        target="_blank"
+      >
+        Link
+      </Link>
+    </Typography>
+  ),
+};
 export const REACT_TO_DO = {
-    title: 'React TO-DO List',
-    text: 'Semplice to-do list sviluppata in React e implementando Firestore.',
-    link: <Typography>
-                <Link style={{color: "#fff", textDecoration: "underline"}} 
-                href="https://thgnommy.github.io/todo-app-react/" 
-                rel="noopener noreferrer" 
-                target="_blank">
-                    Link
-                </Link>
-            </Typography>
-}
+  title: "React TO-DO List",
+  text: "Semplice to-do list sviluppata in React e implementando Firestore.",
+  link: (
+    <Typography>
+      <Link
+        style={{ color: "#fff", textDecoration: "underline" }}
+        href="https://thgnommy.github.io/todo-app-react/"
+        rel="noopener noreferrer"
+        target="_blank"
+      >
+        Link
+      </Link>
+    </Typography>
+  ),
+};
 
 export const SOCIAL_MEDIA_DASHBOARD = {
-    title: 'Social Media Dashboard',
-    text: 'Esempio di una social media dashboard utilizzando HTML e CSS. Esercizio preso di frontendmentor.io',
-    link: <Typography>
-                <Link style={{color: "#fff", textDecoration: "underline"}} 
-                href="https://thgnommy.github.io/social-media-dashboard-with-theme-switcher-master/" 
-                rel="noopener noreferrer" 
-                target="_blank">
-                    Link
-                </Link>
-            </Typography>
-}
+  title: "Social Media Dashboard",
+  text:
+    "Esempio di una social media dashboard utilizzando HTML e CSS. Esercizio preso di frontendmentor.io",
+  link: (
+    <Typography>
+      <Link
+        style={{ color: "#fff", textDecoration: "underline" }}
+        href="https://thgnommy.github.io/social-media-dashboard-with-theme-switcher-master/"
+        rel="noopener noreferrer"
+        target="_blank"
+      >
+        Link
+      </Link>
+    </Typography>
+  ),
+};
